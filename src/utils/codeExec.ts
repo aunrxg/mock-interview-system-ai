409e6aec21eb7ab0a08b7eee8ccb3434340e2219
anuraginbytes <anuragpoddar9484@gmail.com>
2025-03-29 00:01:43 +0530

    fix judge0 api

--- src/utils/codeExec.ts
+++ src/utils/codeExec.ts
@@ -20,11 +20,12 @@ export const runCodeAgainstTestCases = async (
     }
 
     const results = [];
+    console.log("Running code using judge0 api");
 
     for (const testCase of testCases) {
       const { input, expectedOutput } = testCase;
 
-      const response = await axios.post(`${process.env.JUDGE0_API_URL}/submissions`,
+      const response = await axios.post(`${process.env.JUDGE0_API_URL}/submissions/?base64_encoded=false&wait=false`,
         {
           source_code: code,
           language_id: languageMap[language],
@@ -41,19 +42,49 @@ export const runCodeAgainstTestCases = async (
       );
 
       const token = response.data.token;
+      console.log("Token from judge0: ", token);
 
       let executionResult = null;
-      while(!executionResult) {
-        const result = await axios.get(`${process.env.JUDGE0_API_URL}/submissions/${token}`, {
-          headers: {
-            "X-RapidAPI-Key": process.env.JUDGE0_API_KEY,
-            "X-RapidAPI-Host": process.env.JUDGE0_API_HOST,
-          },
-        });
+      let attempts = 0;
+      let maxAttempts = 10;
 
-        if(result.data.status.id >= 3) {
-          executionResult = result.data.stdout?.trim();
+      while(!executionResult && attempts < maxAttempts) {
+        try {
+          const result = await axios.get(`${process.env.JUDGE0_API_URL}/submissions/${token}?base64_encode=false`,
+            {
+              headers: {
+                "X-RapidAPI-Key": process.env.JUDGE0_API_KEY,
+                "X-RapidAPI-Host": process.env.JUDGE0_API_HOST,
+              },
+            });
+
+          if(result.data.status.id >= 3) {
+            executionResult = result.data;
+            console.log("Receiving result from judge0: ", executionResult);
+          } else {
+            console.log("Waiting for judge0 to complete processing...");
+            await new Promise((resolve) => setTimeout(resolve, 100));
+          }
+        } catch (error) {
+          console.error("Error while polling judge0: ", error);
+          break;
         }
+
+        attempts++;
+      }
+      console.log("Got the result from judge0");
+      
+      if (!executionResult) {
+        console.error("Failed to retrieve the result from Judge0 after multiple attempts.");
+        results.push({
+          input,
+          expectedOutput,
+          actualOutput: "",
+          passed: false,
+          status: "Execution Timeout",
+          error: "Judge0 API did not respond with a completed status.",
+        });
+        continue;
       }
 
       results.push({
